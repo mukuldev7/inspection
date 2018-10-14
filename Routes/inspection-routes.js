629116a710907cfeff0ee82eadc7dca805ab73d1
mukul77 <mukuldevrani@gmail.com>
2018-10-15 00:56:56 +0530

    settings-changes-production

--- Routes/inspection-routes.js
+++ Routes/inspection-routes.js
@@ -6,6 +6,7 @@ function body_logger_middleware(req, res, next) {
     console.log("req body \n", JSON.stringify(req.body));
     next();
 }
+router.get("/", (req, res) => { res.send("Back End Working") });
 router.post("/authorise_user", inspection_api.inspection_controller.register_login_user_with_role);
 router.post("/add_inspection", inspection_api.inspection_controller.add_inspection_venue);
 router.post("/paginate_inspection_list", inspection_api.inspection_controller.paginate_inspection_list);
