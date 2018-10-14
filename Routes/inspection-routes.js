c485fc906f1245149a51ab15c2b63160e1f6b702
mukul77 <mukuldevrani@gmail.com>
2018-10-15 00:58:50 +0530

    settings-changes-production

--- Routes/inspection-routes.js
+++ Routes/inspection-routes.js
@@ -4,11 +4,11 @@ const router = express.Router();
 const inspection_api = require("../Controllers/");
 function body_logger_middleware(req, res, next) {
     console.log("req body \n", JSON.stringify(req.body));
-    next();
+    return next();
 }
 router.get("/", (req, res) => { res.send("Back End Working") });
-router.post("/authorise_user", inspection_api.inspection_controller.register_login_user_with_role);
-router.post("/add_inspection", inspection_api.inspection_controller.add_inspection_venue);
-router.post("/paginate_inspection_list", inspection_api.inspection_controller.paginate_inspection_list);
+router.post("/authorise_user", body_logger_middleware, inspection_api.inspection_controller.register_login_user_with_role);
+router.post("/add_inspection", body_logger_middleware, inspection_api.inspection_controller.add_inspection_venue);
+router.post("/paginate_inspection_list", body_logger_middleware, inspection_api.inspection_controller.paginate_inspection_list);
 
 module.exports = router;
